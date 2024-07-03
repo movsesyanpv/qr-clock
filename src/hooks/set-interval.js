84ef1d5896a7ba1519313d4c86b3b185184fa1c2
Pavel Movsesyan <movsesyanpv1995@live.com>
2024-07-03 12:26:25 +0300

    Update set-interval.js

--- src/hooks/set-interval.js
+++ src/hooks/set-interval.js
@@ -1,9 +1,6 @@
-import { useEffect, useRef } from 'react';
 import { useTimer } from "react-use-precision-timer";
 
 export default function useInterval(callback, delay) {
-  const callbackRef = useRef();
-
   const timer = useTimer({ delay: delay }, callback);
   timer.start()
 }
